867fa5925d77db387507a3eb2d094d55d7ee5f6f
Jakub Kopański <jkopansk@gmail.com>
2019-07-09 22:49:45 +0200

    Conditional Applicative helpers

--- packages/overture/src/control/applicative/index.ts
+++ packages/overture/src/control/applicative/index.ts
@@ -16,3 +16,12 @@ export interface Applicative<F, A> extends Apply<F, A> {
 export interface IsApplicative<F> extends TypeFamily<Kind1> {
   (): Applicative<F, this[0]>
 }
+
+export function when (cond: boolean) {
+  return <F extends IsApplicative<F>>(f: Of<F, void>) =>
+    cond ? f : f.constructor.pure(undefined)
+}
+
+export function unless (cond: boolean) {
+  return when(!cond)
+}
