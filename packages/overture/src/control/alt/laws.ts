b5925e62a4660e2e3b32089d7dcdf63984d41662
Jakub Kopański <jkopanski@protonmail.com>
2019-07-12 21:05:22 +0200

    Simplify Alt laws definition to take less arguments

--- packages/overture/src/control/alt/laws.ts
+++ packages/overture/src/control/alt/laws.ts
@@ -7,14 +7,12 @@ import { Fun } from "../../data/function"
 export default function laws<F extends IsAlt<F>, A, B>(
   arbF: Arbitrary<Fun<A, B>>,
   arbX: Arbitrary<Of<F, A>>,
-  arbY: Arbitrary<Of<F, A>>,
-  arbZ: Arbitrary<Of<F, A>>
 ) {
   describe("Alt laws", () => {
     test("associativity", () => {
       assert(
         property(
-          arbX, arbY, arbZ, (x, y, z) => expect(eq(
+          arbX, arbX, arbX, (x, y, z) => expect(eq(
             x.alt(y).alt(z),
             x.alt(y.alt(z))
           )).toBe(true)
@@ -25,7 +23,7 @@ export default function laws<F extends IsAlt<F>, A, B>(
     test("distributivity", () => {
       assert(
         property(
-          arbF, arbX, arbY, (f, x, y) => expect(eq(
+          arbF, arbX, arbX, (f, x, y) => expect(eq(
             x.alt(y).map(f),
             x.map(f).alt(y.map(f))
           )).toBe(true)
