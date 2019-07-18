2d9d04303455593635f3fe7c3a331663dac06256
Jakub Kopański <jkopanski@protonmail.com>
2019-07-18 14:37:39 +0200

    Equality class

---
+++ packages/overture/src/data/eq/index.ts
@@ -0,0 +1,46 @@
+import {
+  Of,
+  Kind1,
+  TypeFamily
+} from "tshkt"
+import { Void } from "../void"
+
+export interface Eq<A> {
+  eq (this: A, a: A): boolean
+}
+
+export interface Eq1<F, A> {
+  eq (this: Of<F, A>, fa: Of<F, A>): boolean
+}
+
+export type HasEq<A> = A extends Eq<A> ? A : Void
+
+export interface IsEq<F> extends TypeFamily<Kind1> {
+  (): Eq1<F, HasEq<this[0]>>
+}
+
+Number.prototype.eq = function (n: number) {
+  return this.valueOf() === n
+}
+
+String.prototype.eq = function (s: string) {
+  return this.valueOf() === s
+}
+
+Boolean.prototype.eq = function (b: boolean) {
+  return this.valueOf() === b
+}
+
+declare global {
+  interface Number {
+    eq (n: number): boolean
+  }
+
+  interface String {
+    eq (s: string): boolean
+  }
+
+  interface Boolean {
+    eq (b: boolean): boolean
+  }
+}
---
+++ packages/overture/src/data/eq/laws.ts
@@ -0,0 +1,47 @@
+import { Arbitrary, assert, pre, property } from "fast-check"
+import { isDeepStrictEqual as eq } from "util"
+import { Eq } from "."
+
+export default function laws<A extends Eq<A>>(
+  arbA: Arbitrary<A>
+) {
+  describe("Eq laws", () => {
+    test("reflexivity", () => {
+      assert(
+        property(
+          arbA, a => expect(
+            a.eq(a)
+          ).toBe(true)
+        )
+      )
+    })
+
+    test("symmetry", () => {
+      assert(
+        property(
+          arbA, arbA, (a, b) =>
+            expect(eq(
+              a.eq(b),
+              b.eq(a)
+            )).toBe(true)
+        )
+      )
+    })
+
+    test("transitivity", () => {
+      assert(
+        property(
+          arbA, arbA, arbA, (a, b, c) => {
+            // this gonna be sloooow
+            pre(a.eq(b))
+            pre(b.eq(c))
+            return expect(
+              a.eq(c)
+            ).toBe(true)
+          }
+        ),
+        { maxSkipsPerRun: 10000 }
+      )
+    })
+  })
+}
