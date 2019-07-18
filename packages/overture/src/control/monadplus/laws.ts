e1768e07c82ab67cab726c4819148fd5629f432a
Jakub Kopański <jkopanski@protonmail.com>
2019-07-18 14:37:56 +0200

    Depend on Eq in MonadPlus laws

--- packages/overture/src/control/monadplus/laws.ts
+++ packages/overture/src/control/monadplus/laws.ts
@@ -1,22 +1,36 @@
 import { Arbitrary, assert, property } from "fast-check"
-import { Of } from "tshkt"
-import { isDeepStrictEqual as eq } from "util"
-import { IsMonadPlus } from "."
+import { Kind1, Of, TypeFamily } from "tshkt"
+import { Eq, Eq1, HasEq } from "../../data/eq"
+import { MonadPlus } from "."
 import { Fun } from "../../data/function"
 
-export default function laws<F extends IsMonadPlus<F>, A, B>(
-  arbF: Arbitrary<Fun<A, Of<F, B>>>,
-  arbX: Arbitrary<Of<F, A>>
+interface ComparableMonadPlus<F, A extends Eq<A>>
+  extends MonadPlus<F, A>,
+    Eq1<F, A> {}
+
+interface IsComparableMonadPlus<F> extends TypeFamily<Kind1> {
+  (): ComparableMonadPlus<F, HasEq<this[0]>>
+}
+
+export default function laws<
+  A,
+  B,
+  F extends IsComparableMonadPlus<F>
+>(
+  arbF: Arbitrary<Fun<HasEq<A>, Of<F, HasEq<B>>>>,
+  arbX: Arbitrary<Of<F, HasEq<A>>>
 ) {
   describe("MonadPlus laws", () => {
     test("distributivity", () => {
       assert(
         property(
-          arbF, arbX, arbX, (f, x, y) => expect(eq(
-            x.alt(y).bind(f),
-            x.bind(f).alt(y.bind(f))
-          )).toBe(true)
-        )
+          arbF, arbX, arbX, (f, x, y) => expect(
+            x.alt(y).bind(f).eq(
+              x.bind(f).alt(y.bind(f))
+            )
+          ).toBe(true)
+        ),
+        { verbose: true }
       )
     })
   })
