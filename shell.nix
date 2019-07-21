c993315710fa3d87458e6e9c63eec0b00db2eca9
Jakub Kopański <jkopanski@protonmail.com>
2019-07-21 15:29:45 +0200

    Init monorepo with lerna

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  config = {};
+  pkgs = import <unstable> { inherit config; };
+  node = pkgs.nodejs-10_x;
+in
+  pkgs.stdenv.mkDerivation {
+    name = "famisoft-env";
+    buildInputs = [
+      node
+      pkgs.nodePackages.lerna
+    ];
+    shellHook = ''
+      export PATH=$PWD/node_modules/.bin:$PATH
+    '';
+  }
