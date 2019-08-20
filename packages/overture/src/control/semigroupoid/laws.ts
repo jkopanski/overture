5ac7e0e1906c668304eefa79e55920ae547128dc
Jakub Kopański <jkopanski@protonmail.com>
2019-08-20 13:36:17 +0200

    Take custom equivalence as semigroupoid laws

--- packages/overture/src/control/semigroupoid/laws.ts
+++ packages/overture/src/control/semigroupoid/laws.ts
@@ -1,6 +1,9 @@
 import { Arbitrary, assert, property } from "fast-check"
 import { Of2 } from "tshkt"
-import { isDeepStrictEqual as eq } from "util"
+
+import { Equivalence } from "../../data/functor/contravariant/equivalence"
+import { tuple } from "../../data/tuple"
+
 import { IsSemigroupoid, compose, pipe } from "."
 
 /**
@@ -22,18 +25,22 @@ export default function laws <
   arbF: Arbitrary<Of2<F, C, D>>,
   arbG: Arbitrary<Of2<F, B, C>>,
   arbH: Arbitrary<Of2<F, A, B>>,
+  arbA: Arbitrary<A>,
+  equiv: (a: A) => Equivalence<Of2<F, A, D>>
 ) {
   describe("Semigroupoid laws", () => {
     test("associativity", () => {
       assert(
         property(
-          arbF, arbG, arbH, (f, g, h) =>
-            expect(
-              eq(
+          arbF, arbG, arbH, arbA, (f, g, h, a) => {
+            const eq = equiv(a).get()
+            return expect(
+              eq(tuple(
                 compose(f, g, h),
                 pipe(h, g, f)
-              )
+              ))
             ).toBe(true)
+          }
         )
       )
     })
